6c9bb399fcdd9009fe80201602d193de8fbf7bde
JorgeTarancon <si.jorgetarancon@gmail.com>
2024-05-15 17:23:29 +0200

    update: add consumption of the model from streamlit app
    Signed-off-by: JorgeTarancon <si.jorgetarancon@gmail.com>

--- streamlit-ui/src/my-wallet-manager.py
+++ streamlit-ui/src/my-wallet-manager.py
@@ -5,6 +5,8 @@ import ast
 import streamlit as st
 import sys
 import gspread
+import joblib
+import pandas as pd
 ######################
 
 
@@ -33,4 +35,20 @@ sh = gc.open(config['GoogleSheets']['spreadsheetName'])
 ws_cuentas = sh.worksheet("CUENTAS_TOTALES")
 
 st.write(ws_cuentas.acell("B2").value)
+
+left_column, right_column = st.columns(2)
+INGRESOS = left_column.slider("Earnings", 0.0, 4000.0, 2600.0)
+YEAR = right_column.slider("YEAR", 2020, 2100, 2024)
+MONTH = left_column.slider("MONTH", 1, 12, 7)
+
+# Cargar el pipeline desde el archivo
+
+model = joblib.load("../models/linearregression.joblib")
+df = pd.DataFrame(
+    data=[[INGRESOS, YEAR, MONTH]],
+    columns=['INGRESOS','YEAR','MONTH'],
+)
+result = model.predict(df)
+
+st.write(f"The estimated spends are: {round(result[0][0], 2)}")
 ######################
